db9b1d57f551017f46da286c77ea916bd31358cc
Félix Ofx <xlfelixgamer@gmail.com>
2026-03-27 17:34:33 -0400

    Actualizar update.ts

--- plugins/dev/update.ts
+++ plugins/dev/update.ts
@@ -1,6 +1,6 @@
 import { exec } from 'child_process'
 import { promisify } from 'util'
-import { loadPlugins, pluginCache } from '../../manipulador.js'
+import { loadPlugins, pluginCache } from '../../manipulador.ts'
 
 const execute = promisify(exec)
 
